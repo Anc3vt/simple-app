d11ed94d86ece95e087013df94ec13fa15964e1f
Ancevt <me@ancevt.com>
2023-01-03 02:54:44 +0300

    Initial commit

---
+++ src/main/java/com/ancevt/teach/simpleapp/Main.java
@@ -0,0 +1,14 @@
+package com.ancevt.teach.simpleapp;
+
+public class Main {
+
+    public static void main(String[] args) {
+        for(String arg : args) {
+
+            arg = arg.toUpperCase();
+
+            System.out.println(arg);
+        }
+    }
+
+}
