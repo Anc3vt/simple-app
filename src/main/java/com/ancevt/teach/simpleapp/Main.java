b03cdd3602082859e07fbd25f969e4468ae244d1
Ancevt <me@ancevt.com>
2023-01-29 14:39:37 +0300

    1

--- src/main/java/com/ancevt/teach/simpleapp/Main.java
+++ src/main/java/com/ancevt/teach/simpleapp/Main.java
@@ -3,12 +3,20 @@ package com.ancevt.teach.simpleapp;
 public class Main {
 
     public static void main(String[] args) {
-        for(String arg : args) {
 
-            arg = arg.toUpperCase();
 
-            System.out.println(arg);
-        }
+
+
+
+
+
+
+
+
+
+
+
+
     }
 
 }
